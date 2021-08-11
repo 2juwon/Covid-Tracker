82bcb8b5211acb39214a3ea940e4d9b359b0059b
devdan <2juwon@gmail.com>
2021-08-11 13:44:05 +0900

    주소 -> 좌표 변환

---
+++ server/crawling/crawling/request.py
@@ -0,0 +1,63 @@
+import json
+import requests
+import urllib3
+import re
+
+urllib3.disable_warnings()
+
+naver_client_info = {
+    "ID" : 'yqzkt8lqam',
+    "SECRET" : 'RtiiXX9xPfpduPA1RWd85AEG65IGaZwv5VUtHESd'
+}
+
+naver_header_map = {
+    "Accept": "application/json",
+    "Content-Type": "application/json;charset=utf-8",
+    "X-NCP-APIGW-API-KEY-ID": naver_client_info["ID"],
+    "X-NCP-APIGW-API-KEY": naver_client_info["SECRET"]
+}
+
+def parse_geocode(address):
+    # print(address)
+
+    query = requests.utils.quote(address)
+    url = f'https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query={query}'
+    data = {
+        "query": address
+    }
+
+    # print(url)
+
+    try:
+        response = requests.get(url, headers=naver_header_map, verify=False, timeout=5)
+
+        try:
+            json_data = json.loads(response.text)
+            addresses = json_data.get("addresses")
+            if(len(addresses) > 0):
+                for address in addresses:
+                    x = address.get("x")
+                    y = address.get("y")
+                    return {'lon' : x, 'lat' : y}
+        except json.decoder.JSONDecodeError as decodeerror:
+            print("JSONDecodeError : ", decodeerror)
+            print("JSON string : ", response.text)
+            
+    except requests.exceptions.Timeout as timeouterror:
+        print("Timeout Error : ", timeouterror)
+
+    except requests.exceptions.SSLError as sslerror:
+        print("SSL Error : ", sslerror)
+        close()
+
+    except requests.exceptions.ConnectionError as connectionerror:
+        print("Connection Error : ", connectionerror)
+        # See psf/requests#5430 to know why this is necessary.                
+
+    except requests.exceptions.HTTPError as httperror:
+        print("Http Error : ", httperror)
+
+    except requests.exceptions.RequestException as error:
+        print("AnyException : ", error)
+
+    return None
---
+++ server/crawling/crawling/test.py
@@ -0,0 +1,4 @@
+from request import parse_geocode
+
+print(parse_geocode('중앙대로29번길 2-11 뚱보집'))
+
